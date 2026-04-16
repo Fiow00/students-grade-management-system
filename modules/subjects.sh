b8670d17cf28f0b26fd973ae07f50d5dd3c91731
Fiow00 <ahmedyhya5325500@gmail.com>
2026-04-16 11:44:03 +0200

    Implement 'manage_subjects', and 'add_subject' functionality

---
+++ modules/subjects.sh
@@ -0,0 +1,100 @@
+manage_subjects() {
+    while true
+    do
+        clear
+        echo "============================"
+        echo " Manage Subjects "
+        echo "============================"
+        echo "1) Add Subject"
+        echo "2) List Subjects"
+        echo "3) Update Subject"
+        echo "4) Delete Subject"
+        echo "b) Back"
+        echo ""
+
+        read -r -p "Enter your selection: " choice
+
+        case "$choice" in
+            1) add_subject ;;
+            2) list_subjects ;;
+            3) update_subject ;;
+            4) delete_subject ;;
+            b) echo "Back to main menu..."; sleep 0.5; break ;;
+            *) echo "Invalid choice!" ;;
+        esac
+
+        echo ""
+        read -p "Press Enter to continue..." _
+    done
+}
+
+
+add_subject() {
+    clear
+    echo "=========================="
+    echo " Add Subject "
+    echo "=========================="
+
+    local subject_code
+
+    # get subject code
+    while true
+    do
+        read -r -p "Enter Subject Code (e.g CS101, MATH203): " subject_code
+
+        if [[ "$subject_code" =~ ^[a-zA-Z]{2,5}[0-9]{2,4}$ ]]
+        then
+            if [[ -f "$SUBJECTS_DIR/${subject_code}.sub" ]]
+            then
+                echo "Error: Subject code '$subject_code' already exists."
+            else
+                break
+            fi
+        else
+            echo "Error: Code must be 2-5 letters followed by 2-4 digits (e.g. CS101)."
+        fi
+    done
+
+    # get subject code
+    local subject_name
+
+    while true
+    do
+        read -r -p "Enter Subject Name: " subject_name
+
+        if [[ -z "$subject_name" ]]
+        then
+            echo "Error: Subject name cannot be empty."
+        elif [[ ! "$subject_name" =~ ^[a-zA-Z[:space:]]+$ ]]
+        then
+            echo "Error: Name must contain letters only."
+        else
+            break
+        fi
+    done
+
+    # get credit hours
+    local subject_credits
+
+    while true
+    do
+        read -r -p "Enter Credit Hours (1-6): " subject_credits
+
+        if [[ "$subject_credits" =~ ^[1-6]$ ]]
+        then
+            break
+        else
+            echo "Error: Credit hours must be an integer between 1 and 6."
+        fi
+    done
+
+    {
+        echo "CODE=$subject_code"
+        echo "NAME=$subject_name"
+        echo "CREDITS=$subject_credits"
+    } > "$SUBJECTS_DIR/${subject_code}.sub"
+
+    echo ""
+    echo "Subject '$subject_name' ($subject_code) added successfully!"
+
+}
